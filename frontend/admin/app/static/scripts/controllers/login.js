90f9a494908b177dea2196fac23c2bc8c09368e0
unknown <rafanetow@gmail.com>
2015-10-25 16:45:49 -0600

    creating  route of login

---
+++ frontend/admin/app/static/scripts/controllers/login.js
@@ -0,0 +1,8 @@
+/*
+;(function(angular) {
+	'use strict';
+angular.module('app.controllers').controller('LoginCtrl', ['$scope',
+		function($scope) {
+
+
+})(angular);
--- frontend/admin/app/static/scripts/routes.js
+++ frontend/admin/app/static/scripts/routes.js
@@ -4,18 +4,19 @@
 	angular.module('app')
 	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 			var viewsPath = '/static/views/';
-			$urlRouterProvider.otherwise('/home');
+			$urlRouterProvider.otherwise('/home/login');
 
 			$stateProvider.
 				state('home',{
 					url: '/home',
 					templateUrl: viewsPath+'home.html'
-			}),
-				state('login',{
-					url: '/login',{
-					templateUrl: viewsPath+'login.html'
-					}
-				})
+			}).
+				state('home.login',{
+					url: '/login',
+					templateUrl: viewsPath+'login.html',
+					requiresAuthentication: false
+
+				});
 
 }]);
 

f6754db1b6ad95eef4be38ec38e2db4a521c6b5e
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-11-02 11:11:12 -0600

    added login logic

--- frontend/admin/app/static/scripts/controllers/login.js
+++ frontend/admin/app/static/scripts/controllers/login.js
@@ -1,11 +1,29 @@
 ;(function(angular) {
 	'use strict';
 
-	angular.module('app.controllers').controller('LoginCtrl', ['$scope','$location',
-		function($scope,$location) {
+	angular.module('app.controllers').controller('LoginCtrl', ['$scope','$location', 'ngToast', 'Auth',
+		function($scope, $location, ngToast, Auth) {
+			$scope.credentials = {
+				identifier: '',
+				password: ''
+			};
 
 			$scope.authenticate = function() {
-				$location.path('/home');
+				Auth.login($scope.credentials).success(function() {
+					$location.path('/home');
+				}).error(function(response) {
+					$scope.credentials.identifier = '';
+					$scope.credentials.password = '';
+
+					var message = response;
+
+					ngToast.create({
+						className: 'danger',
+						content: message,
+						dismissButton: true
+					});
+				});
+
 			};
 
 		}

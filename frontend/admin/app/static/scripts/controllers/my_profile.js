58f8edd5b36e52fdc325d70f76e995255e8ca4b2
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-11-14 22:13:07 -0600

    added change password modal to profile page

--- frontend/admin/app/static/scripts/controllers/my_profile.js
+++ frontend/admin/app/static/scripts/controllers/my_profile.js
@@ -1,8 +1,8 @@
 ;(function(angular) {
 	'use strict';
 
-	angular.module('app.controllers').controller('MyProfileCtrl', ['$scope', 'User',
-		function($scope, User) {
+	angular.module('app.controllers').controller('MyProfileCtrl', ['$scope', '$modal', 'ngToast', 'User',
+		function($scope, $modal, ngToast, User) {
 			$scope.user = null;
 			$scope.isLoading = true;
 			$scope.statusToString = User.statusToString;
@@ -12,6 +12,28 @@
 			}).finally(function() {
 				$scope.isLoading = false;
 			});
+
+			$scope.openChangePasswordModal = function() {
+				var modalInstance = $modal.open({
+					animation: true,
+					controller: 'changePasswordModalCtrl',
+					templateUrl: 'static/views/users/change_password_modal.html',
+					resolve: {
+						user: function() {
+							return {
+								id: $scope.user.id,
+								username: $scope.user.username
+							};
+						}
+					}
+				});
+
+				modalInstance.result.then(function(newPassword) {
+					User.changePassword($scope.user.id, newPassword).success(function() {
+						ngToast.create('Se cambió la clave del usuario!');
+					});
+				});
+			};
 
 		}
 	]);

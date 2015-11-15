175133919adba262d25989889ccece8b6f12b827
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-11-15 10:15:34 -0600

    added controller for the change email modal

---
+++ frontend/admin/app/static/scripts/controllers/users/change_email_modal.js
@@ -0,0 +1,24 @@
+;(function(angular) {
+	'use strict';
+
+	angular.module('app.controllers').controller('changeEmailModalCtrl', ['$scope', '$modalInstance', 'user',
+		function($scope, $modalInstance, user) {
+			$scope.user = user;
+			$scope.email = '';
+
+			$scope.accept = function() {
+				$scope.changeEmailForm.$submitted = true;
+
+				if ($scope.changeEmailForm.$invalid) {
+					return;
+				}
+
+				$modalInstance.close($scope.email);
+			};
+
+			$scope.cancel = function() {
+				$modalInstance.dismiss();
+			};
+		}
+	]);
+})(angular);

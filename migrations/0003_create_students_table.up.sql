50e082b737245a12fbdcac0023cda36d219a9436
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-12-26 12:00:33 -0600

    created students table migrations

---
+++ migrations/0003_create_students_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE students
+(
+  id serial NOT NULL,
+  email character varying(60),
+  first_name character varying(60),
+  last_name character varying(60),
+  status integer,
+  created_at timestamp with time zone,
+  updated_at timestamp with time zone,
+  deleted_at timestamp with time zone,
+  CONSTRAINT users_pkey PRIMARY KEY (id),
+)
+WITH (
+  OIDS=FALSE
+);

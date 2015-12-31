d22a8b3c9e5c8becd76ab5ced38735de78ec31d1
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-12-30 23:50:41 -0600

    created handler to find student by id

--- router/student/student_routes.go
+++ router/student/student_routes.go
@@ -44,3 +44,48 @@ func (r *studentRouter) FindAllAvailable(ctx context.Context, w http.ResponseWri
 
 	return httputils.WriteJSON(w, http.StatusOK, response)
 }
+
+// Find Student by id.
+func (r *studentRouter) FindById(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
+	var (
+		err       error
+		studentId int
+		s, _      = ctx.Value("services").(*services.Services)
+		vars      = mux.Vars(req)
+	)
+
+	type MappedStudent struct {
+		Id        int       `json:"id"`
+		Email     string    `json:"email"`
+		FirstName string    `json:"firstName"`
+		LastName  string    `json:"lastName"`
+		Status    int       `json:"status"`
+		CreatedAt time.Time `json:"createdAt"`
+	}
+
+	studentId, err = strconv.Atoi(vars["id"])
+
+	if err != nil {
+		httputils.WriteError(w, http.StatusBadRequest, "")
+		return err
+	}
+
+	student, err := s.Student.FindById(studentId)
+	if err != nil {
+		return err
+	} else if user == nil {
+		httputils.WriteError(w, http.StatusNotFound, "")
+		return nil
+	}
+
+	response := &MappedStudent{
+		Id:        student.Id,
+		Email:     student.Email,
+		FirstName: student.FirstName,
+		LastName:  student.LastName,
+		Status:    student.Status,
+		CreatedAt: student.CreatedAt,
+	}
+
+	return httputils.WriteJSON(w, http.StatusOK, response)
+}

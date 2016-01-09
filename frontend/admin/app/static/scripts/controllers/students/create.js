a0caf053e01c4b2d41787b39758ab865b17e97c1
unknown <rafanetow@gmail.com>
2016-01-08 22:36:58 -0600

    controller to viewCreateStudent

---
+++ frontend/admin/app/static/scripts/controllers/students/create.js
@@ -0,0 +1,48 @@
+;(function(angular) {
+	'use strict';
+
+	angular.module('app.controllers').controller('CreateStudentCtrl', ['$scope', '$stateParams', '$location', 'ngToast', 'Student',
+		function($scope, $stateParams, $location, ngToast, Student) {
+			$scope.statuses = Student.statuses;
+			$scope.student = {
+				firstName: '',
+				lastName: '',
+				status: 1
+			};
+
+
+			$scope.onStudentnameChange = function() {
+				/*
+				if ($scope.student.firstName === '') {
+					$scope.userForm.username.$setValidity('available', true);
+					return;
+				}
+
+				//missing request to backend
+			*/
+			};
+
+			$scope.createStudent = function() {
+
+
+				Student.create($scope.student).success(function(response) {
+					if (!response.success) {
+						ngToast.create({
+							className: 'danger',
+							content: response.errorMessage,
+							dismissButton: true
+						});
+						return;
+					}
+					ngToast.create('El usuario se ha creado!');
+
+					$scope.student = {
+						firstName: '',
+						lastName: '',
+						status: 1
+					};
+				});
+			};
+		}
+	]);
+})(angular);

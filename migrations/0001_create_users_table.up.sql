d6521509a0d527e25606d23a128e1ce31fe43447
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-01-18 15:35:10 -0600

    added unique index on username

--- migrations/0001_create_users_table.up.sql
+++ migrations/0001_create_users_table.up.sql
@@ -1,20 +1,25 @@
 CREATE TABLE users
 (
-  id serial NOT NULL,
-  password character varying(255),
-  email character varying(60),
-  first_name character varying(60),
-  last_name character varying(60),
-  status integer,
-  created_at timestamp with time zone,
-  updated_at timestamp with time zone,
-  deleted_at timestamp with time zone,
-  username character varying(30),
-  is_admin boolean NOT NULL DEFAULT false,
-  is_teacher boolean NOT NULL DEFAULT false,
-  CONSTRAINT users_pkey PRIMARY KEY (id),
-  CONSTRAINT users_username_lowercase_ck CHECK (username::text = lower(username::text))
+	id serial NOT NULL,
+	password character varying(255),
+	email character varying(60),
+	first_name character varying(60),
+	last_name character varying(60),
+	status integer,
+	created_at timestamp with time zone,
+	updated_at timestamp with time zone,
+	deleted_at timestamp with time zone,
+	username character varying(30),
+	is_admin boolean NOT NULL DEFAULT false,
+	is_teacher boolean NOT NULL DEFAULT false,
+	CONSTRAINT users_pkey PRIMARY KEY (id),
+	CONSTRAINT users_username_lowercase_ck CHECK (username::text = lower(username::text))
 )
 WITH (
-  OIDS=FALSE
-);
+	  OIDS=FALSE
+  );
+
+CREATE UNIQUE INDEX username_unique_idx
+	ON users
+	USING btree
+	(username COLLATE pg_catalog."default");

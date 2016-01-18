4d6d7ac09dfb29d24264d79165b1d9c1f257c722
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-01-18 16:22:25 -0600

    modified create students scripts to add new columns

--- migrations/0003_create_students_table.up.sql
+++ migrations/0003_create_students_table.up.sql
@@ -1,15 +1,27 @@
 CREATE TABLE students
 (
-  id serial NOT NULL,
-  email character varying(60),
-  first_name character varying(60),
-  last_name character varying(60),
-  status integer,
-  created_at timestamp with time zone,
-  updated_at timestamp with time zone,
-  deleted_at timestamp with time zone,
-  CONSTRAINT students_pkey PRIMARY KEY (id)
+	id serial NOT NULL,
+	id_number character varying(40) NOT NULL,
+	email character varying(60),
+	first_name character varying(60),
+	last_name character varying(60),
+	status integer,
+	place_of_birth character varying(60),
+	address character varying(100),
+	birthdate date,
+	gender character(1),
+	nationality character varying(30),
+	phone_number character varying(40),
+	created_at timestamp with time zone,
+	updated_at timestamp with time zone,
+	deleted_at timestamp with time zone,
+	CONSTRAINT students_pkey PRIMARY KEY (id)
 )
 WITH (
-  OIDS=FALSE
+	OIDS=FALSE
 );
+
+CREATE UNIQUE INDEX id_number_unique_idx
+	ON students
+	USING btree
+	(id_number COLLATE pg_catalog."default");

4d5885a86b68b0190c3ab207990ecdd83df8beaf
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-02-15 15:37:06 -0600

    created fn to validate session data after decrypt

--- router/session_data.go
+++ router/session_data.go
@@ -17,3 +17,16 @@ type SessionData struct {
 	IsTeacher bool
 	ExpiresAt time.Time
 }
+
+// IsInvalid checks wether the data is in the correct state.
+func (s *SessionData) IsInvalid() bool {
+	if s.UserID == 0 {
+		return true
+	}
+
+	if s.ExpiresAt.IsZero() {
+		return true
+	}
+
+	return false
+}

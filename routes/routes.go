f7d94d3fb06533cfdeb21232476468aa8ce99a85
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-02-18 19:54:37 -0600

    cleaned routes

--- routes/routes.go
+++ routes/routes.go
@@ -5,13 +5,6 @@ import (
 	"github.com/ab22/abcd/services"
 
 	"github.com/ab22/abcd/handlers/auth"
-	"github.com/ab22/abcd/handlers/cart"
-	"github.com/ab22/abcd/handlers/city"
-	"github.com/ab22/abcd/handlers/country"
-	"github.com/ab22/abcd/handlers/currency"
-	"github.com/ab22/abcd/handlers/event"
-	"github.com/ab22/abcd/handlers/eventtype"
-	"github.com/ab22/abcd/handlers/state"
 	"github.com/ab22/abcd/handlers/static"
 	"github.com/ab22/abcd/handlers/user"
 )
@@ -26,16 +19,9 @@ type Routes struct {
 // and API Routes.
 func NewRoutes(cfg *config.Config, services *services.Services) *Routes {
 	var (
-		staticHandler    = static.NewHandler(cfg)
-		authHandler      = auth.NewHandler(services)
-		cartHandler      = cart.NewHandler(services)
-		cityHandler      = city.NewHandler(services)
-		countryHandler   = country.NewHandler(services)
-		currencyHandler  = currency.NewHandler(services)
-		eventHandler     = event.NewHandler(services)
-		eventTypeHandler = eventtype.NewHandler(services)
-		stateHandler     = state.NewHandler(services)
-		userHandler      = user.NewHandler(services)
+		staticHandler = static.NewHandler(cfg)
+		authHandler   = auth.NewHandler(services)
+		userHandler   = user.NewHandler(services)
 
 		r = &Routes{
 			TemplateRoutes: []Route{},

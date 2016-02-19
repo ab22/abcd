48a3a3a07d1778a1fe635f4970ffba8301fcf815
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-02-18 20:20:53 -0600

    golint fixes

--- handlers/static/static.go
+++ handlers/static/static.go
@@ -28,11 +28,13 @@ func NewHandler(cfg *config.Config) *Handler {
 	return h
 }
 
+// Index simply returns the index.html file from the main app.
+//
 // Since Go's router sends all lost requests to home path '/',
 // then we check if the URL path is not '/'.
 // If the requested URL is '/', then we render the index.html template.
 // If it's not, then we return a 404 response.
-func (r *staticRouter) Index(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
+func (h *Handler) Index(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
 	if req.URL.Path != "/" {
 		http.NotFound(w, req)
 		return nil

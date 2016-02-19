d8fe901b67acdba65c731cc69cd6e727a7cb0afa
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-02-19 10:46:47 -0600

    moved session data to httputils package

--- httputils/session_data.go
+++ httputils/session_data.go
@@ -1,12 +1,6 @@
-package handlers
+package httputils
 
 import "time"
-
-const (
-	// SessionCookieName is the name used to read the session cookie from the
-	// client.
-	SessionCookieName = "_session"
-)
 
 // SessionData describes the session cookie for all users.
 type SessionData struct {

17392332a9bfc72a9ad52a76ef346ce7992bd158
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-03-08 14:23:27 -0600

    comment fix

--- handlers/static/static.go
+++ handlers/static/static.go
@@ -10,7 +10,7 @@ import (
 	"golang.org/x/net/context"
 )
 
-// Handler defines all methods to be implemented by the Auth Handler.
+// Handler defines all methods to be implemented by the Static Handler.
 type Handler interface {
 	Index(context.Context, http.ResponseWriter, *http.Request) error
 }

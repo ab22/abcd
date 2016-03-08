df3d598330b5fb692b8c35c4456a70f54e062a4f
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-03-08 14:28:23 -0600

    defined user interface

--- handlers/user/user.go
+++ handlers/user/user.go
@@ -12,8 +12,24 @@ import (
 	"golang.org/x/net/context"
 )
 
-// Handler structure for the user handlers.
-type Handler struct {
+// Handler defines all methods to be implemented by the User Handler.
+type Handler interface {
+	FindAllAvailable(context.Context, http.ResponseWriter, *http.Request) error
+	FindByID(context.Context, http.ResponseWriter, *http.Request) error
+	FindByUsername(context.Context, http.ResponseWriter, *http.Request) error
+	Create(context.Context, http.ResponseWriter, *http.Request) error
+	Edit(context.Context, http.ResponseWriter, *http.Request) error
+	ChangePassword(context.Context, http.ResponseWriter, *http.Request) error
+	Delete(context.Context, http.ResponseWriter, *http.Request) error
+	GetProfileForCurrentUser(context.Context, http.ResponseWriter, *http.Request) error
+	ChangePasswordForCurrentUser(context.Context, http.ResponseWriter, *http.Request) error
+	ChangeEmailForCurrentUser(context.Context, http.ResponseWriter, *http.Request) error
+	ChangeFullNameForCurrentUser(context.Context, http.ResponseWriter, *http.Request) error
+	GetPrivilegesForCurrentUser(context.Context, http.ResponseWriter, *http.Request) error
+}
+
+// handler structure for the user handlers.
+type handler struct {
 	services services.Services
 }
 

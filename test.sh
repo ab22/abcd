67e955f8d30b346a83928a4875f2d9c017900f7b
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-03-12 15:08:44 -0600

    added test bash script

--- test.sh
+++ test.sh
@@ -1,3 +1,16 @@
+# test.sh takes a list of packages, appends them to a whole string and sends
+# them to go test.
+#
+# Since using 'go test ./...' will take the vendor folder as a valid package
+# path, it will attempt to test all of the vendored packages. Also, since
+# this project contains a frontend folder with all of it's frontend
+# npm/bower modules, 'go test ./...' will also scan those folders taking up
+# more time for the test run to complete.
+#
+# Note: If a new package is added with tests, it has to be added to the
+#       PACKAGE_LIST array.
+
+
 PACKAGE_LIST=(
 	"github.com/ab22/abcd"
 	"github.com/ab22/abcd/config"
